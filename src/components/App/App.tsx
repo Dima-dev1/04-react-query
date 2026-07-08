7aad6ca3c9c7fdeb47facce26e68745b24138257
Dima <66467881+Dima-dev1@users.noreply.github.com>
2026-07-08 16:56:41 +0100

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
+import css from "./App.module.css";
+
+function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSearch = async (query: string) => {
+    try {
+      setMovies([]);
+      setIsError(false);
+      setIsLoading(true);
+
+      const results = await fetchMovies(query);
+
+      if (results.length === 0) {
+        toast.error("No movies found for your request.");
+        return;
+      }
+
+      setMovies(results);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleSelect = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+  };
+
+  return (
+    <div className={css.app}>
+      <SearchBar onSubmit={handleSearch} />
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={handleSelect} />
+      )}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import "modern-normalize/modern-normalize.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import App from "./components/App/App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+    <Toaster position="top-right" />
+  </StrictMode>,
+);
